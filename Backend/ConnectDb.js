c90c75384c57a2e89bc084997136c0249af9af4e
Bhoomitiwari <151936313+Bhoomitiwari@users.noreply.github.com>
2025-01-23 17:29:56 +0530

    Update ConnectDb.js

--- Backend/ConnectDb.js
+++ Backend/ConnectDb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');  // Make sure mongoose is required
 const connectDB = async () => {
     await mongoose.connect(
-        'mongodb+srv://vivpasingh:ielBXbmVKP18id56@cluster0.xew7a.mongodb.net/Newsify'
+        'Your_URL'
     );
     console.log('MongoDB Connected...');
 };
